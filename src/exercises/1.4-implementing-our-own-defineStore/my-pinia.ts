4a87cf8110af001b83066054f1def4df2974e348
xiaoluabei <xiaoluabei@gmail.com>
2025-01-12 12:34:48 +0800

    exercise 1

--- src/exercises/1.4-implementing-our-own-defineStore/my-pinia.ts
+++ src/exercises/1.4-implementing-our-own-defineStore/my-pinia.ts
@@ -1,3 +1,16 @@
-export function defineStore() {
-  // TODO: see instructions.md
+import { effectScope } from "vue";
+
+
+const stores = new WeakMap<() => unknown, unknown>()
+const globalScope = effectScope(true)
+export function defineStore<R>(fn: () => R) {
+
+  function useStore() {
+    if (!stores.has(fn)) {
+      const store = globalScope.run(() => effectScope().run(fn))
+      stores.set(fn, store)
+    }
+    return stores.get(fn) as R
+  }
+  return useStore
 }
