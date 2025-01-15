2f8cb75c49952de201dde62f6601338b1b3eb023
xiaoluabei <xiaoluabei@gmail.com>
2025-01-15 12:36:21 +0800

    6.5.2-define-private-state

--- src/exercises/6.5.2-define-private-state/private-state.ts
+++ src/exercises/6.5.2-define-private-state/private-state.ts
@@ -1,14 +1,22 @@
 // NOTE: remove this line (or change the 1 into 0) if you don't want
 // to work on the Type Safety part
-/* eslint @typescript-eslint/no-explicit-any:1 */
-import { defineStore } from 'pinia'
-import { reactive } from 'vue'
+/* eslint @typescript-eslint/no-explicit-any:0 */
+import { defineStore, StateTree } from 'pinia'
 
-export function definePrivateState(id: any, privateStateFn: any, setup: any) {
+export function definePrivateState<Id extends string, PrivateState extends () => StateTree, SetupReturn>(
+  id: Id,
+  privateStateFn: PrivateState,
+  setup: (privateSTate: ReturnType<PrivateState>) => SetupReturn,
+) {
+  const usePrivateStore = defineStore(`${id}-private`, {
+    state: privateStateFn,
+  })
+
+  const privateStore = usePrivateStore()
   return defineStore(id, () => {
     return setup(
       // FIXME: you will have to rewrite this to make it work
-      reactive({ n: 0 }),
+      privateStore.$state as any,
     )
   })
 }
